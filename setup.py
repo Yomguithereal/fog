81e4ae391851c8b6c6442e79860c0a0962203263
Yomguithereal <guillaumeplique@gmail.com>
2018-04-27 14:09:49 +0200

    Correct setup.py?

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('./README.md', 'r') as f:
+    long_description = f.read()
 
 setup(name='fog',
       version='0.0.1',
       description='A fuzzy matching & clustering library for python.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/Yomguithereal/fog',
       license='MIT',
       author='Guillaume Plique',
-      packages=[
-        'fog'
-      ],
+      keywords='fuzzy',
+      python_requires='>=3',
+      packages=find_packages(exclude=['test']),
+      package_data={'docs': ['README.md']},
       zip_safe=True)
