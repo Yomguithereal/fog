74d829683bcf22e23c9f9772623832b4b2bcbbbd
Yomguithereal <guillaumeplique@gmail.com>
2018-06-08 18:28:28 +0200

    Documenting key collision

--- fog/clustering/key_collision.py
+++ fog/clustering/key_collision.py
@@ -7,16 +7,24 @@
 from collections import defaultdict
 
 # TODO: multiple keys per item
-# TODO: better docs
 
 
 def key_collision(data, key, min_size=2, max_size=float('inf')):
     """
     Function returning an iterator over found clusters.
 
-    It works by grouping the given items by keys in a multimap.
+    It works by grouping items in buckets if they share the same key, as
+    returned by a key function. Useful key functions that can efficiently
+    regroup similar strings range from case normalization to phonetic encodings
+    to clever fingerprint functions such as the one used by OpenRefine.
 
-    It therefore runs in O(n).
+    Since it runs in O(n), it is one of the fastest record-linkage clustering
+    algorithm. However, it won't be able to match more subtle string
+    differences such as typos etc.
+
+    Note that the key function can return more than one key for the string.
+    In that case, buckets can be merged or not and produce slightly different
+    clusters.
 
     Args:
         data (iterable): Arbitrary iterable containing data points to gather
