76dfc85a95ac222bdfb4dd6448a0ec4ac9565e4f
Yomguithereal <guillaumeplique@gmail.com>
2018-06-20 16:26:51 +0200

    Better rusalka

--- fog/key/rusalka.py
+++ fog/key/rusalka.py
@@ -5,6 +5,7 @@
 # Rusalka is a standardization algorithm for transliterated Russian names.
 #
 import re
+from unidecode import unidecode
 
 FILTER = re.compile(r'[^a-z]')
 DUPLICATED = re.compile(r'(.)\1+')
@@ -13,8 +14,12 @@ VOWELS = re.compile(r'[aeiouy]')
 RULES = [
     (r'i[jy]$', ''),
     (r'[fvw]', 'f'),
-    (r'[dt]?[cs]h', 'ʃ'),
+    (r'[dt]?[cs]h|tz', 'ʃ'),
     (r'[dt]?(?:zh|j)', 'ʒ'),
+    (r'ks', 'x'),
+    (r'^y?e', 'j'),
+    (r'^y(?=[aeiou])|iou', 'j'),
+    (r'[cg]', 'k'),
     (r'h', '')
 ]
 
@@ -22,7 +27,12 @@ RULES = [(re.compile(p), r) for p, r in RULES]
 
 
 def rusalka(name):
+
+    # Lower case
     name = name.lower()
+
+    # Unidecode
+    name = unidecode(name)
 
     # Dropping irrelevant characters
     name = re.sub(FILTER, '', name)
@@ -34,7 +44,14 @@ def rusalka(name):
     # Deduplication
     name = re.sub(DUPLICATED, r'\1', name)
 
-    # Dropping vowels
-    name = re.sub(VOWELS, '', name)
+    first_letter = name[0]
+    rest = name[1:]
 
-    return name
+    # Dropping vowels
+    rest = re.sub(VOWELS, '', rest)
+
+    # Handling first letter
+    if re.match(VOWELS, first_letter):
+        first_letter = 'a'
+
+    return first_letter + rest
--- test/key/rusalka_test.py
+++ test/key/rusalka_test.py
@@ -6,8 +6,28 @@ from fog.key import rusalka
 
 TESTS = [
     (('Tchekov', 'Chekhow', 'Tchekof', 'Tchekoff'), 'ʃkf'),
-    (('Dzhugashvili', 'Dzhougachvili', 'Djougachvili'), 'ʒgʃfl'),
-    (('Dimitrij', 'Dimitri', 'Dimitry', 'Dimitriy', 'Dmitri', 'D\'mitr', 'Dmitr'), 'dmtr')
+    (('Dzhugashvili', 'Dzhougachvili', 'Djougachvili'), 'ʒkʃfl'),
+    (('Dimitrij', 'Dimitri', 'Dimitry', 'Dimitriy', 'Dmitri', 'D\'mitr', 'Dmitr'), 'dmtr'),
+    (('Alexei', 'Alexey'), 'alx'),
+    (('Sergei', 'Sergey'), 'srk'),
+    (('Ekaterina', 'Yekaterina'), 'jktrn'),
+    (('Moussorgsky', 'Musorkgski', 'mousorgskiy', 'Moußorgsky'), 'msrksk'),
+    (('Manovich', 'Manovitz', 'Manovitch'), 'mnfʃ'),
+    (('Yeltsin', 'Eltsine'), 'jltsn'),
+    (('Konstantine', 'Constantine'), 'knstntn'),
+    (('Poutine', 'Putin'), 'ptn'),
+    (('Diaghilev', 'Diaghileff', 'Diagilef'), 'dklf'),
+    (('Plushchenko', 'Plushchenko'), 'plʃnk'),
+    (('Rusalka', 'Roussalka'), 'rslk'),
+    (('Piotr', 'Pyotr', 'Pyter'), 'ptr'),
+    (('Gagarin', 'Gagarine'), 'kkrn'),
+    (('Illich', 'Ilitch'), 'alʃ'),
+    (('Vladimir', 'Wladimir'), 'fldmr'),
+    (('Mikhail', ), 'mkl'),
+    (('Onéguine', 'Oniéguine', 'Onegin'), 'ankn'),
+    (('Bakhtin', ), 'bktn'),
+    (('Youri', 'Yuri', 'Iouri'), 'jr'),
+    (('Aleksandr', 'Alexander'), 'alxndr')
 ]
 
 
