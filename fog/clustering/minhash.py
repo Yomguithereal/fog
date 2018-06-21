8b046bb3f4546e157a01edfd28fec4f0862ae8ed
Yomguithereal <guillaumeplique@gmail.com>
2018-06-21 14:33:19 +0200

    Adding some docs

--- fog/clustering/minhash.py
+++ fog/clustering/minhash.py
@@ -20,21 +20,60 @@ from fog.metrics.jaccard import jaccard_similarity
 #   * possibility to hash the band key
 #   * note that we allow uneven bands for fine grained results
 #   * double_check with minhash or jaccard or sub similarity even
-#   * superminhash to generate signature faster
+#   * superminhash to generate signature faster (better for large docs)
 #   * cheap_hashes
-#   * docs
 #   * possibility to use one dict per band + sum the integers
 
 
 def match_probability(h, bands, similarity):
+    """
+    Function returning the probability two pairs will match given a number
+    of a signature's integers, the number of bands dividing the signature
+    matrix and the desired similarity.
+
+    Args:
+        h (int): Number of integers in the minhash signature.
+        bands (int): Number of bands dividing the signature matrix.
+        similarity (float): Desired Jaccard similarity.
+
+    Returns:
+        float: The match probability.
+
+    """
     return 1.0 - (1.0 - similarity ** (h / bands)) ** bands
 
 
 def similarity_threshold(h, bands):
+    """
+    Function returning the Jaccard similarity threshold for minhash signature
+    composed of h integers and a signature matrix divided in n bands.
+
+    Args:
+        h (int): Number of integers in the minhash signature.
+        bands (int): Number of bands dividing the signature matrix.
+
+    Returns:
+        float: The Jaccard similarity threshold.
+
+    """
     return (1.0 / bands) ** (1 / (h / bands))
 
 
 def guess_bands(h, threshold):
+    """
+    Function used to iteratively guess the optimal number of bands needed to
+    divide a minhash signature matrix in order to find pairs having a
+    Jaccard similarity over the given threshold.
+
+    Args:
+        h (int): Number of integers in the minhash signature.
+        threshold (float): Jaccard similarity threshold.
+
+    Returns:
+        int: The optimal number of bands.
+
+    """
+
     bands = 1
 
     while bands <= h:
@@ -48,7 +87,26 @@ def guess_bands(h, threshold):
     return bands
 
 
-def minhash(data, h=256, key=None, radius=0.8, bands=None):
+def minhash(data, h=256, key=None, radius=0.8, bands=None, use_numpy=False):
+    """
+    Function returning an iterator over clusters found using the minhash
+    clustering method.
+
+    Args:
+        data (iterable): Items to cluster.
+        h (int, optional): Number of integers to use as the minhash signature.
+            Defaults to 256.
+        key (callable, optional): Function returning an item's key.
+        radius (float, optional): Radius over which a pair of items is deemed
+            similar. Defaults to 0.8.
+        bands (int, optional): By defaults, the function will attempt to guess
+            the optimal number of bands to use to divide the signature matrix
+            using given radius. Set this argument if you want to set the
+            number of bands by yourself.
+        use_numpy (bool, optional): whether to use numpy to speed up minhash
+            signatures computations. Defaults to False.
+
+    """
 
     if bands is None:
         bands = guess_bands(h, radius)
@@ -56,7 +114,7 @@ def minhash(data, h=256, key=None, radius=0.8, bands=None):
     rows = h // bands
     h_upper_bound = bands * rows
 
-    mh = MinHash(h)
+    mh = MinHash(h, use_numpy=use_numpy)
 
     buckets = defaultdict(list)
 
