4ce43bca77323a14560df2eab7a8daca293ea623
Yomguithereal <guillaumeplique@gmail.com>
2018-06-28 14:03:36 +0200

    Better CLI

--- fog/cli/cluster.py
+++ fog/cli/cluster.py
@@ -6,42 +6,90 @@
 # a CSV file.
 #
 import csv
-from collections import defaultdict
-
-from fog.cli.ui.cluster import ClusteringUI
+import re
+from collections import Counter
+from datetime import datetime
+from timeit import default_timer as timer
 
 from fog.clustering import (
     key_collision
 )
 from fog.key import fingerprint
 
-
-def fingerprint_collision(data):
-    clusters = key_collision(data, key=fingerprint)
-
-    return list(clusters)
-
-
 CLUSTERING_ROUTINES = {
     'fingerprint_collision': {
         'name': 'Fingerpint collision',
-        'fn': fingerprint_collision
+        'fn': key_collision,
+        'args': {
+            'key': fingerprint
+        }
     }
 }
+
+
+def escape_quote(string):
+    return string.replace('\'', '\\\'')
+
+
+def print_toml_report(meta, values, clusters):
+    print('[info]')
+    print('date = %s' % meta['date'].isoformat().split('.')[0])
+    print('algorithm = \'%s\'' % meta['algorithm'])
+    print()
+    print('[stats]')
+    print('lines = %i' % meta['lines'])
+    print('nb_distinct_values = %i' % len(values))
+    print('nb_clusters = %i' % len(clusters))
+    print('took = %2f' % meta['took'])
+    print()
+
+    for i, cluster in enumerate(clusters):
+        print('[[cluster]]')
+        print('id = %i' % i)
+        print('nb_values = %i' % len(cluster))
+
+        # Sorting by affected rows
+        sorted_values = sorted(cluster, key=lambda v: values[v], reverse=True)
+        max_length = len(max(cluster, key=len))
+
+        print('harmonized = \'%s\'' % escape_quote(sorted_values[0]))
+        print('values =  [')
+        for value in sorted_values:
+            print('  [\'%s\',%s %i],' % (
+                escape_quote(value),
+                ' ' * (max_length - len(value)),
+                values[value]
+            ))
+
+        print(']')
+
+        print('harmonize = false')
+
+        print()
 
 
 def cluster_action(namespace):
     routine = CLUSTERING_ROUTINES[namespace.algorithm]
 
+    lines = 0
     with open(namespace.file, 'r') as f:
         reader = csv.DictReader(f)
 
-        rows = defaultdict(list)
+        values = Counter()
 
         for line in reader:
-            rows[line[namespace.column]].append(line)
+            lines += 1
+            values[line[namespace.column]] += 1
 
-    clusters = routine['fn'](rows.keys())
+    start = timer()
+    clusters = routine['fn'](values.keys(), **routine['args'])
+    end = timer() - start
 
-    ui = ClusteringUI(rows, clusters)
-    ui.run()
+    meta = {
+        'algorithm': namespace.algorithm,
+        'took': end,
+        'date': datetime.now(),
+        'lines': lines
+    }
+
+    print_toml_report(meta, values, list(clusters))
