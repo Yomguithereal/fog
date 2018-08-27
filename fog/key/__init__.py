e54be2f051cbe195d8842b35ec7c3537a630a368
Yomguithereal <guillaumeplique@gmail.com>
2018-08-27 14:47:36 +0200

    Adding misc keys

--- fog/key/__init__.py
+++ fog/key/__init__.py
@@ -5,5 +5,6 @@ from fog.key.fingerprint import (
     ngrams_fingerprint
 )
 from fog.key.levenshtein_1d import levenshtein_1d, damerau_levenshtein_1d
+from fog.key.misc import zig_zag
 from fog.key.omission import omission_key
 from fog.key.skeleton import skeleton_key
---
+++ fog/key/misc.py
@@ -0,0 +1,9 @@
+# =============================================================================
+# Fog Miscellaneaous Keys
+# =============================================================================
+#
+# Miscellaneous set of key functions used for blocking and/or sorted
+# neighborhood approaches.
+#
+
+zig_zag = (None, lambda x: x[::-1])
