7fb56334cfac07f2b1bb47b0ea7ad69aa26e7ad3
Yomguithereal <guillaumeplique@gmail.com>
2019-04-10 16:49:27 +0200

    Adding passjoin template

---
+++ fog/clustering/passjoin.py
@@ -0,0 +1,21 @@
+# =============================================================================
+# Fog PassJoin Clustering
+# =============================================================================
+#
+# Implementation of the PassJoin algorithm.
+#
+# [References]:
+# Jiang, Yu, Dong Deng, Jiannan Wang, Guoliang Li, et Jianhua Feng.
+# « Efficient Parallel Partition-Based Algorithms for Similarity Search and Join
+# with Edit Distance Constraints ». In Proceedings of the Joint EDBT/ICDT 2013
+# Workshops on - EDBT ’13, 341. Genoa, Italy: ACM Press, 2013.
+# https://doi.org/10.1145/2457317.2457382.
+#
+# Li, Guoliang, Dong Deng, et Jianhua Feng. « A Partition-Based Method for
+# String Similarity Joins with Edit-Distance Constraints ». ACM Transactions on
+# Database Systems 38, no 2 (1 juin 2013): 1‑33.
+# https://doi.org/10.1145/2487259.2487261.
+#
+# [Urls]:
+# http://people.csail.mit.edu/dongdeng/projects/passjoin/index.html
+#
