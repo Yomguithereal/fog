556cd162c46e2161ce5a4d5a9b149d7b5757b6a3
Yomguithereal <guillaumeplique@gmail.com>
2020-10-01 10:43:15 +0200

    Drafting fog.graph

---
+++ fog/graph/projection.py
@@ -0,0 +1,10 @@
+# =============================================================================
+# Fog Graph Projections
+# =============================================================================
+#
+# Miscellaneous functions related to bipartite to monopartite projections.
+#
+
+
+def cosine_monopartite_projection():
+    pass
