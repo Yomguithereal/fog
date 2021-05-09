22070856811dc0dc4e700e437a1e2ffc1dd87813
Yomguithereal <guillaumeplique@gmail.com>
2021-05-09 22:24:11 +0200

    Bump 0.11.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ EXTENSIONS = [
 ]
 
 setup(name='fog',
-    version='0.11.0',
+    version='0.11.1',
     description='A fuzzy matching & clustering library for python.',
     long_description=long_description,
     long_description_content_type='text/markdown',
